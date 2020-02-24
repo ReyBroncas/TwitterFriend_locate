94000b84edc44fcc3187bedacd970144fa89c933
ReyBroncas <ReyBroncas@github.com>
2020-02-24 22:33:44 +0200

    recleaned

--- location.py
+++ location.py
@@ -7,7 +7,7 @@ def generate_map(users_data):
         folium.Marker(v['location'], popup=f"<img src='{v['image']}' \
          alt='Avatar' style='height:20px; width:20px'>",
                       tooltip=user).add_to(m)
-    m.save('templates/friendsmap.html')
+    m.save('templates/temp.html')
     with open('templates/friendsmap.html', 'r') as file:
         htmls = file.read()
     return htmls
